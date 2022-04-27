0598c118f402ad96d9da63a4a99dd84c072c52bd
Aung Moe Kyaw <87358448+AungMoeKyaw135@users.noreply.github.com>
2022-04-27 08:57:26 +0630

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import cv2
 from tensorflow.keras.models import load_model
 import streamlit as st
 
-#MODEL_DIR = os.path.join(os.path.dirname(__file__), 'model')
-#if not os.path.isdir(MODEL_DIR):
-#    os.system('runipy Final.ipynb')
+MODEL_DIR = os.path.join(os.path.dirname(__file__), 'model')
+if not os.path.isdir(MODEL_DIR):
+    os.system('runipy Final.ipynb')
 
-#model = load_model('model')
+model = load_model('model')
 # st.markdown('<style>body{color: White; background-color: DarkSlateGrey}</style>', unsafe_allow_html=True)
 
 st.title('Traffic Sign Detection')
@@ -23,18 +23,18 @@ Try Anything
 
 uploaded_file = st.file_uploader("Choose a file")
 
-if st.button('Try'):
-    st.write("Done")
+#if st.button('Try'):
+#    st.write("Done")
 
-#if uploaded_file is not None:
-#     
-#    img = cv2.resize(uploaded_file, (150, 150))
-#    rescaled = cv2.resize(img, (150, 150), interpolation=cv2.INTER_NEAREST)
-#    st.write('Model Input')
-#    st.image(rescaled)
+if uploaded_file is not None:
+     
+    img = cv2.resize(uploaded_file, (150, 150))
+    rescaled = cv2.resize(img, (150, 150), interpolation=cv2.INTER_NEAREST)
+    st.write('Model Input')
+    st.image(rescaled)
 
-#if st.button('Predict'):
-#    test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#    val = model.predict(test_x.reshape(1, 150, 150))
-#    st.write(f'result: {np.argmax(val[0])}')
-#    st.bar_chart(val[0])
+if st.button('Predict'):
+    test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    val = model.predict(test_x.reshape(1, 150, 150))
+    st.write(f'result: {np.argmax(val[0])}')
+    st.bar_chart(val[0])
