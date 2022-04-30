9c4b15cfa0dcea3804f39cb6551c48df085c90bb
Aung Moe Kyaw <87358448+AungMoeKyaw135@users.noreply.github.com>
2022-04-30 21:45:35 +0630

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,9 @@ uploaded_file = st.file_uploader("Choose a file")
 #    st.write("Done")
 
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    img = cv2.imread(bytes_data)
+    #bytes_data = uploaded_file.getvalue()
+    string_data = stringio.read()
+    img = cv2.imread(string_data)
     #rescaled = cv2.resize(img, (150, 150), interpolation=cv2.INTER_NEAREST)
     st.write('Model Input')
     st.image(img)
