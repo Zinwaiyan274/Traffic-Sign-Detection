b821ba9c735fcdcb112fdefc29c6041f2fd4a056
Aung Moe Kyaw <87358448+AungMoeKyaw135@users.noreply.github.com>
2022-04-30 10:22:50 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-import pandas as pd
+#import pandas as pd
 import numpy as np
 import cv2
 from tensorflow.keras.models import load_model
@@ -25,16 +25,16 @@ uploaded_file = st.file_uploader("Choose a file")
 #    st.write("Done")
 
 if uploaded_file is not None:
-    bytes_data = uploaded_file.getvalue()
-    #img = cv2.resize(uploaded_file, (150, 150))
-    #rescaled = cv2.resize(img, (150, 150), interpolation=cv2.INTER_NEAREST)
+    #bytes_data = uploaded_file.getvalue()
+    img = cv2.resize(uploaded_file, (150, 150))
+    rescaled = cv2.resize(img, (150, 150), interpolation=cv2.INTER_NEAREST)
     st.write('Model Input')
-    st.image(uploaded_file)
+    st.image(rescaled)
 
 if st.button('Predict'):
-    #test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #val = model.predict(bytes_data)
-    #st.write(f'result: {np.argmax(val[0])}')
-    #st.bar_chart(val[0])
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    test_x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    val = model.predict(test_x)
+    st.write(f'result: {np.argmax(val[0])}')
+    st.bar_chart(val[0])
+    #dataframe = pd.read_csv(uploaded_file)
+    #st.write(dataframe)
