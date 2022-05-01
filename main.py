3b189f27baee368f5d97892f8a0ef074dbfd1a43
Aung Moe Kyaw <87358448+AungMoeKyaw135@users.noreply.github.com>
2022-05-01 09:19:33 +0630

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if uploaded_file is not None:
     st.write("")
     st.write("Classifying...")
     label,perc = teachable_machine_classification(image, 'Final.h5')
-    st.write(label)
+    #st.write(label)
     df = pd. DataFrame(label, columns=['Giveway', 'NoEntry','NoHorn','Roundabout','Stop']) 
     st.write(df)
     
