18fb29905201128d145d9795eb252d07de41348e
Aung Moe Kyaw <87358448+AungMoeKyaw135@users.noreply.github.com>
2022-05-01 08:25:51 +0630

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ if uploaded_file is not None:
     st.write("Classifying...")
     label,perc = teachable_machine_classification(image, 'Final.h5')
     st.write(label)
-    label =np.array(a,b)
+    a,b =np.array(label)
     st.write(a,b)
     if label.any() == 0:
      st.write("Giveway",perc)
